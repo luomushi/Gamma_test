861ff5a2fc3e2f37f5e3665917e90b150afe493e
luomushi <129146600+luomushi@users.noreply.github.com>
2023-03-29 14:22:39 +0800

    Update conf.py

--- net_demo/source/conf.py
+++ net_demo/source/conf.py
@@ -36,7 +36,7 @@ language = 'zh_CN'
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_copy_source=False  # 关闭右上角的查看源码
+html_copy_source=True  # 关闭右上角的查看源码
 
 html_favicon = '../picture/favicon.jpg'
 
